b7cc39d1712de44d19cc817c94f208e2ba55a7f6
FooterReal <footerreal@gmail.com>
2026-03-28 21:16:05 +0100

    Setup basic Web API + Swagger

--- SoulsServer/Program.cs
+++ SoulsServer/Program.cs
@@ -1,5 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUi(options =>
+    {
+        options.DocumentPath = "/openapi/v1.json";
+    });
+}
 
 app.MapGet("/", () => "Hello World!");
 
