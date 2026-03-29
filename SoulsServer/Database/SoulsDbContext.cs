7b81982feb0a3e4a75be282b99a4d635c5ac52ab
FooterReal <footerreal@gmail.com>
2026-03-29 08:49:16 +0200

    Created DbContext

---
+++ SoulsServer/Database/SoulsDbContext.cs
@@ -0,0 +1,9 @@
+namespace SoulsServer.Database;
+
+using Microsoft.EntityFrameworkCore;
+using SoulsServer.Models;
+
+public class SoulsDbContext(DbContextOptions<SoulsDbContext> options) : DbContext(options)
+{
+    public DbSet<Soul> Souls { get; set; }
+}
