558c0219c047f5ed794a8f4abf6eb0337b817ffa
FooterReal <footerreal@gmail.com>
2026-03-29 08:49:36 +0200

    Added DbContext to ASP.NET services

--- SoulsServer/Program.cs
+++ SoulsServer/Program.cs
@@ -1,16 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using SoulsServer.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services
+    .AddDbContextPool<SoulsDbContext>(options => options.UseNpgsql(connectionString))
+    .AddOpenApi()
+    .AddOpenApiDocument();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.UseSwaggerUi(options =>
-    {
-        options.DocumentPath = "/openapi/v1.json";
-    });
+    app.UseOpenApi();
+    app.UseSwaggerUi();
 }
 
 app.MapGet("/", () => "Hello World!");
